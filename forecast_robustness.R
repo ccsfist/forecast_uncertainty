534882a231c74465c2abd048d83e864b44bd6ffa
mm5330 <mm5330@columbia.edu>
2025-08-07 16:27:07 -0400

    Updated analysis file; added figures; cleaned up old abstract

--- forecast_robustness.R
+++ forecast_robustness.R
@@ -1,119 +1,79 @@
 
-library(dplyr)
-library(RJSONIO)
-library(curl)
-library(abind)
-library(sp)
-library(tidyr)
-library(httr)
-library(ggplot2)
 
+## This file generates estimates of forecast uncertainty for the BAMS paper 
+# "Quantifying Uncertainty in Forecast-Based Anticipatory Action Triggers Using Bootstrapping Methods"
+## code written by Max Mauerman (max.mauerman@columbia.edu)
+
+
+source("~/iri-fist/aa_dashboard_common_code/aa_functions.R")
+setwd("~/forecast_uncertainty")
+
+options(timeout=1000000000000)
 
 month_names <- c("jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec")
 
-### SETUP - CHANGE STUFF HERE ###
+### SETUP  ###
 
 ## set what data you'd like to download
 
 # country / project
 
-country <- c("niger")
+countries <- c("madagascar","ethiopia","niger","guatemala-jja","djibouti-ond")
 
 # freqs (0-100)
 
-freqs <- seq(5,95,by=5)
+freqs <- c(20,30)
+
+# season (1 or 2)
+
+season <- 'season1'
 
 # months (numbered 0-11)
 
-months <- c(0:5) 
-
+months_list <- list(c(8, 9, 10),c(11, 0, 1),c(3, 4, 5),c(2, 3, 4),c(6, 7, 8)) 
 months_df <- data.frame("month" = c(0:11),"name" = month_names)
+prev_months_df <- data.frame("month" = c(2:11,0,1),"name" = month_names)
 
-prev_months_df <- data.frame("month" = c(1:11,0),"name" = month_names)
+# define dataset names
 
-# obs dataset (see config for list of possible options)
-
-obs_dataset <- c("chirps-precip-jas")
+forecast_list <- c("pnep-v3","pnep-v5","prcp-v5","pnep-v6","pnep-v2")
+bad_years_list <- c("bad-years-v3","bad-years","bad-years-v3","bad-years","bad-years-rank-v2") 
 
 # server (e.g. production or test)
 
 server <- "https://iridl.ldeo.columbia.edu/fbfmaproom2/"
 
-## get key guide
+i <- 1
+for (country in countries) {
 
-# admin 0
-
-admin_0_key <- bind_rows(fromJSON(paste0(server,"regions?country=",country,"&level=0")))
-
-## function to set query paths 
-
-fun_download_path <- function(country,level,region,month,freq,predictor,predictand){
+  ## get key file
   
-  query_path <- paste0(server,country,"/export?",
-                       "&mode=",level,
-                       "&region=",region,
-                       "&season=season1&issue_month0=",month,
-                       "&freq=",freq,
-                       "&predictor=",predictor,
-                       "&predictand=",predictand) 
-  return(query_path)
+  admin_0_key <- get_key(server,country,0)
   
+  ### DOWNLOADS ###
+  
+  admin_0_rain <- download_data_national(server=server,country=country,key=admin_0_key[1,1],season=season,months=months_list[[i]],
+                                         freqs=freqs,predictor=forecast_list[i],predictand=bad_years_list[i])
+  
+  admin_0_triggers <- compile_data_national(forecast=admin_0_rain,badyears=admin_0_rain,ndvi=NA,prevseas=NA)
+  
+  bootstrap_results <- trigger_uncertainty(admin_0_triggers,5)
+  
+  plot <- ggplot(bootstrap_results,aes(y=new_threshold,x=month)) + 
+    facet_grid(cols=vars(freq)) +
+    geom_boxplot()  +
+    geom_point(aes(y=old_threshold,x=month),color='red') + 
+    geom_text(data = bootstrap_results %>% group_by(month,freq) %>% summarise_all(max), aes(y=old_threshold,x=month,label=round(old_threshold,2)),color='red',alpha=0.7,vjust=-0.5) +
+    geom_hline(aes(yintercept = as.numeric(freq)),linetype='dashed') +
+    theme_bw() +
+    ylim(c(0,50)) +
+    xlab("Issue month") +
+    ylab("Prob. non-exceedence") +
+    ggtitle(paste0(country," trigger uncertainty range"),subtitle = "AA threshold in red; clim. odds dotted line")
+  
+  png(file=paste0(getwd(),"/figures/",country,".png"),width=650,height=400)
+  print(plot)
+  dev.off()
+  
+  i <- i + 1
 }
-
-### DOWNLOADS ###
-
-## admin 0
-
-admin_0_downloads <- list()
-
-for (freq in freqs) {
-  
-  admin_0_downloads[[as.character(freq)]] <- list()
-
-  for (month in months) {
-    
-    api_download <- fromJSON(fun_download_path(country,0,admin_0_key[1,1],month,freq,"pnep",obs_dataset[1]),nullValue = NA)
-    admin_0_downloads[[as.character(freq)]][[as.character(month)]] <- api_download
-    
-
-  }
-  
-}
-
-admin_0_bad_years <- fromJSON(fun_download_path(country,0,admin_0_key[1,1],0,freq,"pnep",'bad-years-v2'),nullValue = NA)
-
-
-bootstrap <- function(obj,n_omit) {
-  
-  years <- unique(obj$year)
-  
-  omit_index <- round(runif(n_omit,min=1,max=length(years)))
-  
-  years_select <- years[-omit_index]
-  
-  obj_select <- obj %>% filter(year %in% years_select) %>%
-    group_by(month,severity) %>%
-    summarise(new_threshold = quantile(forecast,1-(max(freq,na.rm=T)/100),na.rm=T),old_threshold = max(threshold,na.rm=T),
-              freq = max(freq,na.rm=T)) %>%
-    ungroup()
-  
-  return(obj_select)
-  
-}
-
-bootstrap_results <- lapply(c(1:100),function(x) bootstrap(admin_0_triggers,5))
-bootstrap_results <- bind_rows(bootstrap_results)
-
-ggplot(bootstrap_results,aes(y=new_threshold,x=month)) + 
-  facet_grid(cols=vars(severity)) +
-  geom_boxplot()  +
-  geom_point(data=admin_0_triggers %>% filter(year == max(year)),aes(y=forecast,x=month),color='blue',alpha=0.7) +
-  geom_text(data=admin_0_triggers %>% filter(year == max(year)),aes(y=forecast,x=month,label=round(forecast,2)),color='blue',alpha=0.7,vjust=-0.5) +
-  geom_point(aes(y=old_threshold,x=month),color='red') + 
-  geom_text(data = bootstrap_results %>% group_by(month,freq) %>% summarise_all(max), aes(y=old_threshold,x=month,label=round(old_threshold,2)),color='red',alpha=0.7,vjust=-0.5) +
-  geom_hline(aes(yintercept = freq),linetype='dashed') +
-  theme_bw() +
-  xlab("Issue month") +
-  ylab("Prob. non-exceedence") +
-  ggtitle("Trigger uncertainty range",subtitle = "AA threshold in red; current forecast in blue; clim. odds dotted line")
-
