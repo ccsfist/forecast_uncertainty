5c1db490ad5e67217a7daca9bef48394d2fb2d4f
mm5330 <mm5330@columbia.edu>
2025-09-04 14:07:31 -0400

    Added data and bootstrap results as file outputs; organized folders

--- forecast_robustness_madagascar.R
+++ forecast_robustness_madagascar.R
@@ -18,11 +18,11 @@ month_names <- c("jan","feb","mar","apr","may","jun","jul","aug","sep","oct","no
 
 # country / project
 
-countries <- c("madagascar","ethiopia","niger","guatemala-jja","djibouti-ond")
+countries <- c("madagascar")
 
 # freqs (0-100)
 
-freqs <- c(20,30)
+freqs <- c(15)
 
 # season (1 or 2)
 
@@ -30,14 +30,14 @@ season <- 'season1'
 
 # months (numbered 0-11)
 
-months_list <- list(c(8, 9, 10),c(11, 0, 1),c(3, 4, 5),c(2, 3, 4),c(6, 7, 8)) 
+months_list <- list(c(8, 9, 10)) 
 months_df <- data.frame("month" = c(0:11),"name" = month_names)
 prev_months_df <- data.frame("month" = c(2:11,0,1),"name" = month_names)
 
 # define dataset names
 
-forecast_list <- c("pnep-v3","pnep-v5","prcp-v5","pnep-v6","pnep-v2")
-bad_years_list <- c("bad-years-v3","bad-years","bad-years-v3","bad-years","bad-years-rank-v2") 
+forecast_list <- c("pnep-v3")
+bad_years_list <- c("enacts-precip-all") 
 
 # server (e.g. production or test)
 
@@ -46,8 +46,6 @@ server <- "https://iridl.ldeo.columbia.edu/fbfmaproom2/"
 i <- 1
 for (country in countries) {
 
-  print(paste0("Running for ",country))
-  
   ## get key file
   
   admin_0_key <- get_key(server,country,0)
@@ -59,39 +57,38 @@ for (country in countries) {
   
   admin_0_triggers <- compile_data_national(forecast=admin_0_rain,badyears=admin_0_rain,ndvi=NA,prevseas=NA)
   
-  select_data <- admin_0_triggers %>% filter(month == max(month), freq == max(freq)) 
-  
-  write.csv(select_data,paste0(getwd(),"/1. data/",country,".csv"),row.names = FALSE)
-  
+  # select_data <- admin_0_triggers %>% filter(month == max(month), freq == max(freq)) 
+  select_data <- admin_0_triggers
   bootstrap <- trigger_uncertainty(select_data,5) 
   
   colors <- c("Cumulative Forecasts, 1983-present"="black",
               "Empirical Trigger Threshold"="red",
               "Bootstrapped Probability of Passing Threshold" = "blue",
-              "Current Forecast"="green")
+              "Driest Years"="darkgreen")
   
-  current_forecast <- select_data$forecast[select_data$year == max(select_data$year)]
-  forecast_cdf <- ecdf(select_data$forecast)
-  current_pctile = forecast_cdf(current_forecast)
+  years_tohighlight <- select_data %>% 
+    group_by(month) %>%
+    mutate(pctile=ecdf(forecast)(forecast)) %>%
+    filter(badyear==1)
+  
   freq_shown <- max(admin_0_triggers$freq)
                                            
   plot <- ggplot(select_data,aes(x=forecast)) + stat_ecdf(aes(color="Cumulative Forecasts, 1983-present")) + 
+    facet_wrap(~month) +
     stat_ecdf(data=bootstrap,aes(x=new_threshold,color='Bootstrapped Probability of Passing Threshold')) +
     geom_vline(data=bootstrap,aes(xintercept = old_threshold,color='Empirical Trigger Threshold'),linetype='dashed') +
     geom_rect(xmin=quantile(bootstrap$new_threshold,0.10),xmax=quantile(bootstrap$new_threshold,0.90),ymin=Inf,ymax=-Inf,fill='blue',alpha=0.01) +
-    geom_point(x=current_forecast,y=current_pctile,size=3,aes(color='Current Forecast')) +
+    geom_point(data=years_tohighlight,aes(x=forecast,y=pctile,color='Driest Years')) +
+    geom_text(data=years_tohighlight,aes(x=forecast,y=pctile,label=year,color='Driest Years'),size=4,hjust=1,vjust=-1) +
     scale_color_manual(values=colors) +
     labs(x = "Forecast Strength", y = "Cumulative Probability", color = "Legend") +
     scale_y_continuous(labels = scales::percent) +
     ggtitle(paste0("Forecast Trigger Uncertainty Range For ",country),subtitle = paste0("for ",freq_shown,"th percentile drought event")) +
-    xlim(c(0,100)) 
+    xlim(c(0,100)) +
+    theme(legend.position = 'bottom')
   
-  png(file=paste0(getwd(),"/2. figures/",country,".png"),width=650,height=350)
   print(plot)
-  dev.off()
-  
-  write.csv(bootstrap,paste0(getwd(),"/3. results/",country,".csv"),row.names = FALSE)
-  
+
   i <- i + 1
 }
 
